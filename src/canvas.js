eb971b10fbef4596853d43bc951727e3435d4611
Neil Dunn <n768d465@ku.edu>
2017-01-05 13:19:17 -0600

    added canvas colors, random guest names

--- src/canvas.js
+++ src/canvas.js
@@ -1,7 +1,20 @@
 var canvas = new fabric.Canvas('c');
 canvas.isDrawingMode = true;
 
-canvas.freeDrawingBrush.width = 3;
+canvas.freeDrawingBrush.width = 4;
+
+var changeColor = $('#colors');
+var changeWidth = $('#lineWidth');
+
+colors.onchange = function(){
+	canvas.freeDrawingBrush.color = this.value;
+	console.log(this.color);
+}
+
+lineWidth.onchange = function(){
+    canvas.freeDrawingBrush.width = parseInt(this.value, 10) || 1;
+    this.previousSibling.innerHTML = this.value;	
+}
 
 $('#btnClearCanvas').click(function(){
 	socket.emit('draw', canvas.clear());
--- src/client.js
+++ src/client.js
@@ -1,69 +1,74 @@
-    var socket = io();
-    var userName = prompt("Welcome! Enter your name: ", "guest_user");
-    var userLabel = document.getElementById('lblUsername');
+var socket = io();
+var userName = prompt("Welcome! Enter your name: ", generateRandomUser());
+var userLabel = document.getElementById('lblUsername');
 
-    userLabel.innerHTML = "Your username:  " + userName;
-    var userNameToChat = userLabel.innerHTML.slice(15, userLabel.length)
+userLabel.innerHTML = "Your username:  " + userName;
+var userNameToChat = userLabel.innerHTML.slice(15, userLabel.length)
 
-	socket.on('user_joined', function(msg){
-		$('#exampleTextarea').append(msg + '\n')
-	});
+socket.on('user_joined', function(msg){
+	$('#txtAreaChat').append(msg + '\n')
+});
 	
-	socket.on('user_left', function(msg){
-		$('#exampleTextarea').append(msg + '\n')
-	});
+socket.on('user_left', function(msg){
+	$('#txtAreaChat').append(msg + '\n')
+});
 	
-    socket.on('chat message',function (msg){
-      $('#exampleTextarea').append(msg + '\n');
-    });
+socket.on('chat message',function (msg){
+	$('#txtAreaChat').append(msg + '\n');
+});
 		
 	
-	socket.on('del_user', function(name){
-		refreshPlayerList(name);
-	})
+socket.on('del_user', function(name){
+	refreshPlayerList(name);
+});
 	
-	socket.on('add_user', function(name){
-		refreshPlayerList(name);
-	});
+socket.on('add_user', function(name){
+	refreshPlayerList(name);
+});
 	
 
-	socket.on('draw', function(data){
-		canvas.loadFromJSON(data);
-		canvas.renderAll();
-	});
+socket.on('draw', function(data){
+	canvas.loadFromJSON(data);
+	canvas.renderAll();
+});
 	
-	socket.emit('user_joined', "[Server]" + userNameToChat + " has joined the game!");
-	socket.emit('add_user', userNameToChat);
+socket.emit('user_joined', "[Server]" + userNameToChat + " has joined the game!");
+socket.emit('add_user', userNameToChat);
 
-	$(window).on('beforeunload', function(){
-        socket.emit('del_user', userNameToChat);
-		socket.emit('user_left', "[Server]" + userNameToChat + " has left the game.")
+$(window).on('beforeunload', function(){
+    socket.emit('del_user', userNameToChat);
+	socket.emit('user_left', "[Server]" + userNameToChat + " has left the game.")
+});
+	
+$('form').submit(function(){
+	socket.emit('chat message', updateTime() + $('#txtChat').val());
+	$('#txtChat').val('');
+	return false;
     });
 	
-    $('form').submit(function(){
-      socket.emit('chat message', updateTime() + $('#txtChat').val());
-      $('#txtChat').val('');
-      return false;
-    });
-
-
-	canvas.observe('mouse:up', function(){
-		console.log('mouse up');
-		socket.emit('draw', JSON.stringify(canvas));
-	});
+canvas.observe('mouse:up', function(){
+	console.log('mouse up');
+	socket.emit('draw', JSON.stringify(canvas));
+});
 	
-	function updateTime(){
-      var date = new Date();
-      var time = date.toLocaleTimeString();
-      var message = "[" + time + "]" + userNameToChat + ": ";
-      return message;
-    }
+function updateTime(){
+	var date = new Date();
+	var time = date.toLocaleTimeString();
+	var message = "[" + time + "]" + userNameToChat + ": ";
+	return message;
+}
 
 	
-	function refreshPlayerList(name){
-		$('.list-group-item').remove();
+function refreshPlayerList(name){
+	$('.list-group-item').remove();
 		
-		for(var i = 0; i < name.length; i++){
-			$('#listPlayers').append($('<li class = "list-group-item">').text(name[i].username));
-		}
+	for(var i = 0; i < name.length; i++){
+		$('#listPlayers').append($('<li class = "list-group-item">').text(name[i].username + " " + "(" + name[i].points + ")"));
 	}
+}
+
+function generateRandomUser(){
+	var num = (Math.floor((Math.random() * 100) + 1)).toString();
+	var user = "guest_user" + num;
+	return user;
+}
